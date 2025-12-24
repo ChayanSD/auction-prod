879c22e506c469f5db81096d2d4eb82b8e64376c
Chayan SD <chayansd5656@gmail.com>
2025-12-24 12:39:58 +0600

    fix env import

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
+import { env } from "./env";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
     apiVersion : "2025-12-15.clover"
 });
 
